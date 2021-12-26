ef1754e02f9270610a2432406024a12aef5c5fcd
Daniel Martins <_daniel_@hotmail.com.br>
2021-12-26 15:44:53 -0300

    adicionado virgula quando haver mais de 1 idioma e parametrizado o layout

--- src/components/CardPaises.vue
+++ src/components/CardPaises.vue
@@ -1,55 +1,71 @@
 <template>
-<div class="row match-height">
+  <div class="row match-height">
     <div class="col-md-6 col-lg-4 mb-3" v-for="pais in paises" :key="pais.nome">
-    <div class="card card-developer-meetup shadow">
+      <div class="card card-developer-meetup shadow">
         <!----><!---->
         <div class="bg-light-primary rounded-top text-center p-2 bg-dark">
-            <img :src="pais.flags.png" alt="Meeting Pic" width="100%" height="170" class="">
+          <img
+            :src="pais.flags.png"
+            alt="Meeting Pic"
+            width="100%"
+            height="170"
+            class=""
+          />
         </div>
         <div class="card-body">
-            <div class="media">
-                <div class="media-body">
-                    <span>Nome: <span class="fw-bold">{{pais.name.common}}</span></span>
-                    <hr class="m-0 p-0">
-                    <span>Capital: <span class="fw-bold">{{(pais.capital ? pais.capital[0] : 'Nenhum registro')}}</span></span> 
-                    <hr class="m-0 p-0">
-                    <span>Região: <span class="fw-bold">{{pais.region}}</span></span> 
-                    <hr class="m-0 p-0">
-                    <span>SubRegião: <span class="fw-bold">{{pais.subregion}}</span></span>  
-                    <hr class="m-0 p-0">
-                    <span>Idioma(s): <span class="fw-bold">{{formatarIdioma(pais.languages)}} </span> </span> 
-                    <hr class="m-0 p-0">
-                    <span>Lat: <span class="fw-bold">{{pais.latlng[0]}}</span> Log: <span class="fw-bold">{{pais.latlng[1]}}</span> </span> 
-                    <hr class="m-0 p-0">
-                    <span>Ver no mapa: <a target="_blank" :href="pais.maps.googleMaps"><b-icon-flag class="fs-5 mt-2"></b-icon-flag></a></span> 
-                
-                </div>
+          <div class="media">
+            <div class="media-body">
+              <span
+                >Nome: <span class="fw-bold">{{ pais.name.common }}</span></span
+              >
+              <hr class="m-0 p-0" />
+              <span
+                >Capital:
+                <span class="fw-bold">{{
+                  pais.capital ? pais.capital[0] : "Nenhum registro"
+                }}</span></span
+              >
+              <hr class="m-0 p-0" />
+              <span
+                >Região: <span class="fw-bold">{{ pais.region }}</span></span
+              >
+              <hr class="m-0 p-0" />
+              <span
+                >SubRegião:
+                <span class="fw-bold">{{ pais.subregion }}</span></span
+              >
+              <hr class="m-0 p-0" />
+              <span
+                >Idioma(s):
+                <span
+                  class="fw-bold"
+                  v-for="(idioma, key, value) in pais.languages"
+                  :key="key"
+                  >{{ value >= 1 ? `, ${idioma}` : idioma }}
+                </span>
+              </span>
+              <hr class="m-0 p-0" />
+              <span
+                >Lat: <span class="fw-bold">{{ pais.latlng[0] }}</span> Log:
+                <span class="fw-bold">{{ pais.latlng[1] }}</span>
+              </span>
+              <hr class="m-0 p-0" />
+              <span
+                >Ver no mapa:
+                <a target="_blank" :href="pais.maps.googleMaps"
+                  ><b-icon-flag class="fs-5 mt-2"></b-icon-flag></a
+              ></span>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'CardPaises',
-    props: ['paises'],
-    methods: {
-        formatarIdioma(valor){
-
-            let returnIdiomas = [];
-
-            if(valor.lenght > 1) {
-                for(let i = 0; i < valor.lenght; ++i)
-                {
-                    returnIdiomas += valor[i];
-                }
-            }
-            else
-                returnIdiomas = valor;
-            return returnIdiomas;
-        }
-    }
-}
+  name: "CardPaises",
+  props: ["paises"],
+};
 </script>
--- src/views/Paises.vue
+++ src/views/Paises.vue
@@ -1,85 +1,116 @@
 <template>
-    <div class="container">
-        <div class="d-flex align-items-center p-3 mt-4 text-white bg-purple rounded shadow-sm">
-            <button type="button" v-b-modal.modal-dados-brasil class="btn btn-light dados-brasil text-success fs-6 text-uppercase">Dados do Brasil <img src="../assets/svg/brazil.svg" alt="Brasil" width="30px" height="20px"></button>
-            <b-form-select v-model="selected" class="form-control dados-regiao">
-                <b-form-select-option :value="null" disabled selected>Selecione uma região </b-form-select-option>
-]               <b-form-select-option v-for="region in regions" :value="region.continente" :key="region.continente">{{region.nome}}</b-form-select-option>
-            </b-form-select>
-            <div class="form-group has-search">
-                <span class="fa fa-search form-control-feedback"></span>
-                <input type="text" class="form-control" placeholder="Procurar">
-            </div>
-        </div>
-        <b-modal id="modal-dados-brasil" size="lg" title="Dados do Brasil">
-            <h5>teste</h5>
-        </b-modal>
-        <div class="dados-regiao-container mt-3">
-            <CardPaises :paises="dadosRegiao"/>
-        </div>
+  <div class="container">
+    <div
+      class="
+        d-flex
+        align-items-center
+        p-3
+        mt-4
+        text-white
+        bg-purple
+        rounded
+        shadow-sm
+      "
+    >
+      <button
+        type="button"
+        v-b-modal.modal-dados-brasil
+        class="btn btn-light dados-brasil text-success fs-6 text-uppercase"
+      >
+        Dados do Brasil
+        <img
+          src="../assets/svg/brazil.svg"
+          alt="Brasil"
+          width="30px"
+          height="20px"
+        />
+      </button>
+      <b-form-select v-model="selected" class="form-control dados-regiao">
+        <b-form-select-option :value="null" disabled selected
+          >Selecione uma região
+        </b-form-select-option>
+        <b-form-select-option
+          v-for="region in regions"
+          :value="region.continente"
+          :key="region.continente"
+          >{{ region.nome }}</b-form-select-option
+        >
+      </b-form-select>
+      <div class="form-group has-search">
+        <input type="text" class="form-control" placeholder="Procurar" />
+        <b-icon-search></b-icon-search>
+      </div>
     </div>
+    <b-modal id="modal-dados-brasil" size="lg" title="Dados do Brasil">
+      <h5>teste</h5>
+    </b-modal>
+    <div class="dados-regiao-container mt-3">
+      <CardPaises :paises="dadosRegiao" />
+    </div>
+  </div>
 </template>
 <script>
+import CardPaises from "@/components/CardPaises.vue";
 
-import CardPaises from '@/components/CardPaises.vue';
-
- export default{
-     name: "Paises",
-     components: {
-         CardPaises
-     },
-     data() {
-        return {
-            selected: null,
-            dadosRegiao: [],
-            regions: [
-                {
-                    nome:'Africa',
-                    continente: 'Africa'
-                },
-                {
-                    nome:'Americas',
-                    continente: 'Americas'
-                },
-                {
-                    nome: 'Asia',
-                    continente: 'Asia'
-                },
-                {
-                    nome: 'Europa',
-                    continente: 'Europe'
-                },
-                {
-                    nome: 'Oceania',
-                    continente: 'Oceania'
-                }
-            ]
-        }
-      },
-      methods: {
-          getDadosRegiao(regiao){
-              fetch(`https://restcountries.com/v3.1/region/${regiao}`)
-              .then(r => r.json())
-              .then(r => {
-                this.dadosRegiao = r;
-              });
-          }
-      },
-      watch: {
-          selected(region){
-              this.getDadosRegiao(region);
-          }
-      }
- }
+export default {
+  name: "Paises",
+  components: {
+    CardPaises,
+  },
+  data() {
+    return {
+      selected: null,
+      dadosRegiao: [],
+      regions: [
+        {
+          nome: "Africa",
+          continente: "Africa",
+        },
+        {
+          nome: "Americas",
+          continente: "Americas",
+        },
+        {
+          nome: "Asia",
+          continente: "Asia",
+        },
+        {
+          nome: "Europa",
+          continente: "Europe",
+        },
+        {
+          nome: "Oceania",
+          continente: "Oceania",
+        },
+      ],
+    };
+  },
+  methods: {
+    getDadosRegiao(regiao) {
+      this.isLoading = true;
+      fetch(`https://restcountries.com/v3.1/region/${regiao}`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.dadosRegiao = r;
+        });
+      this.isLoading = false;
+    },
+  },
+  watch: {
+    selected(region) {
+      this.getDadosRegiao(region);
+    },
+  },
+};
 </script>
 
 <style scoped>
 .bg-purple {
-    background-color: rgb(48, 149, 142);
+  background-color: rgb(48, 149, 142);
 }
 
 .dados-regiao {
-    width: 180px;
-    margin: auto;
+  width: 180px;
+  margin: auto;
 }
 </style>
