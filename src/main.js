b601634a4b8544291046f120d8bd2943cfb2f049
Daniel Martins <_daniel_@hotmail.com.br>
2021-12-29 19:26:47 -0300

    validação do formulário

--- src/main.js
+++ src/main.js
@@ -5,17 +5,20 @@ import router from './Router.js';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+Vue.use(VueSweetalert2);
 
 import Loading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/vue-loading.css";
-
-Vue.use(BootstrapVue);
-Vue.use(IconsPlugin);
-Vue.use(VueSweetalert2);
 Vue.use(Loading)
+
+import VueTheMask from 'vue-the-mask'
+Vue.use(VueTheMask)
+
 
 Vue.component('loading', Loading);
 
--- src/views/Formulario.vue
+++ src/views/Formulario.vue
@@ -13,87 +13,182 @@
             label="Data de nascimento:"
             label-for="dataDeNascimento"
           >
-            <b-form-datepicker
-              placeholder="dd/mm/yyyy"
-              required
-            ></b-form-datepicker>
+            <validation-provider
+              name="Data de Nascimento"
+              :rules="{
+                required: true,
+                regex:
+                  /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
+              }"
+              v-slot="{ errors, classes }"
+            >
+              <b-form-input
+                v-model="dataDeNascimento"
+                :class="classes"
+                placeholder="dd/mm/yyyy"
+                v-mask="'##/##/####'"
+              ></b-form-input>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
           </b-form-group>
         </b-col>
         <b-col md="4" sm="12">
-          <b-form-group label="Nome:" label-for="nome">
-            <b-form-input placeholder="Ex: João Silva" required></b-form-input>
+          <b-form-group label="Nome completo:" label-for="nome">
+            <validation-provider
+              name="Nome"
+              rules="required|alpha_spaces|min:2"
+              v-slot="{ errors, classes }"
+            >
+              <b-form-input
+                :class="classes"
+                placeholder="Ex: João Silva"
+                v-model="nome"
+              ></b-form-input>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
           </b-form-group>
         </b-col>
         <b-col md="4" sm="12">
           <b-form-group label="CPF:" label-for="cpf">
-            <b-form-input placeholder="123.456.789-09" required></b-form-input>
+            <validation-provider
+              name="CPF"
+              :rules="{ required: true, regex: /^\d{3}.\d{3}.\d{3}-\d{2}$/ }"
+              v-slot="{ errors, classes }"
+            >
+              <b-form-input
+                :class="classes"
+                v-model="cpf"
+                placeholder="123.456.789-09"
+                v-mask="'###.###.###-##'"
+              ></b-form-input>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
+          </b-form-group>
+        </b-col>
+        <b-col md="4" sm="12" class="mt-2">
+          <b-form-group label="Espécie do pet:" label-for="especie">
+            <select class="form-select" aria-label="Default select example">
+              <option selected disabled>Selecione uma opção</option>
+              <option v-for="pet in pets" :key="pet.value">
+                {{ pet.text }}
+              </option>
+            </select>
+          </b-form-group>
+        </b-col>
+        <b-col md="4" sm="12" class="mt-2">
+          <b-form-group label="Raça do pet:" label-for="cpracaf">
+            <select class="form-select" aria-label="Default select example">
+              <option selected disabled>Selecione uma opção</option>
+              <option v-for="pet in pets" :key="pet.value">
+                {{ pet.text }}
+              </option>
+            </select>
+          </b-form-group>
+        </b-col>
+        <b-col md="4" sm="12" class="mt-2">
+          <b-form-group label="Renda mensal:" label-for="renda">
+            <validation-provider
+              name="Renda Mensal"
+              :rules="{ required: true, min_value: 1000 }"
+              v-slot="{ errors, classes }"
+            >
+              <b-input-group prepend="R$">
+                <b-form-input
+                  :class="classes"
+                  v-model="rendaMensal"
+                  v-money="money"
+                  placeholder="0,00"
+                ></b-form-input>
+              </b-input-group>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
           </b-form-group>
         </b-col>
         <b-col md="4" sm="12" class="mt-2">
           <b-form-group label="CEP:" label-for="cep">
-            <b-form-input
-              placeholder="00000-000"
-              v-model="cep"
-              @keyup="buscaCep()"
-              maxlength="8"
-              required
-            ></b-form-input>
+            <validation-provider
+              name="CEP"
+              :rules="{ required: true, regex: /^\d{5}-\d{3}$/ }"
+              v-slot="{ errors, classes }"
+            >
+              <b-form-input
+                id="input-cep"
+                :class="classes"
+                v-model="cep"
+                placeholder="00000-000"
+                v-mask="'#####-###'"
+              ></b-form-input>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
           </b-form-group>
         </b-col>
         <b-col md="4" sm="12" class="mt-2">
-          <b-form-group label="Endereço:" label-for="rua">
-            <b-form-input
-              placeholder="Digite seu endereço"
-              required
-              :value="logradouro"
-            ></b-form-input>
+          <b-form-group label="Rua:" label-for="rua">
+            <validation-provider
+              name="Rua"
+              rules="required"
+              v-slot="{ errors, classes }"
+            >
+              <b-form-input
+                :class="classes"
+                placeholder="Digite seu endereço"
+                v-model="logradouro"
+              ></b-form-input>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
           </b-form-group>
         </b-col>
-        <b-col md="2" sm="12" class="mt-2">
-          <b-form-group label="Cidade:" label-for="cidade">
-            <b-form-input
-              placeholder="Digite sua cidade"
-              require
-              :value="cidade"
-            ></b-form-input>
-          </b-form-group>
-        </b-col>
-        <b-col md="2" sm="12" class="mt-2">
+        <b-col md="4" sm="12" class="mt-2">
           <b-form-group label="Bairro:" label-for="bairro">
-            <b-form-input
-              placeholder="Digite seu bairro"
-              require
-              :value="bairro"
-            ></b-form-input>
+            <validation-provider
+              name="Bairro"
+              rules="required"
+              v-slot="{ errors, classes }"
+            >
+              <b-form-input
+                :class="classes"
+                placeholder="Digite seu bairro"
+                v-model="bairro"
+              ></b-form-input>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
           </b-form-group>
         </b-col>
-        <b-col md="1" sm="12" class="mt-2">
+        <b-col md="4" sm="12" class="mt-2">
+          <b-form-group label="Cidade:" label-for="cidade">
+            <validation-provider
+              name="Cidade"
+              rules="required"
+              v-slot="{ errors, classes }"
+            >
+              <b-form-input
+                :class="classes"
+                placeholder="Digite sua cidade"
+                v-model="cidade"
+              ></b-form-input>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
+          </b-form-group>
+        </b-col>
+        <b-col md="4" sm="12" class="mt-2">
           <b-form-group label="Estado:" label-for="estado">
-            <select class="form-select" aria-label="Default select example">
-              <option></option>
-              <option v-for="uf in ufs" :key="uf.value">{{ uf.text }}</option>
-            </select>
+            <validation-provider
+              name="Estado"
+              rules="required|alpha|max:2"
+              v-slot="{ errors, classes }"
+            >
+              <b-form-input
+                :class="classes"
+                placeholder="Ex: GO"
+                v-model="estado"
+                maxlength="2"
+              ></b-form-input>
+              <span class="mensagem-error">{{ errors[0] }}</span>
+            </validation-provider>
           </b-form-group>
         </b-col>
       </b-row>
 
-      <h3 class="mt-3">Pet</h3>
-      <hr class="mb-2 mt-0" />
-
-      <b-row>
-        <b-col md="6" sm="12">
-          <select class="form-select" aria-label="Default select example">
-            <option selected disabled>Selecione uma opção</option>
-            <option v-for="pet in pets" :key="pet.value">{{ pet.text }}</option>
-          </select>
-        </b-col>
-        <b-col md="6" sm="12">
-          <select class="form-select" aria-label="Default select example">
-            <option selected disabled>Selecione uma opção</option>
-            <option v-for="pet in pets" :key="pet.value">{{ pet.text }}</option>
-          </select>
-        </b-col>
-      </b-row>
       <div class="mt-3">
         <b-button type="submit" variant="primary">Salvar</b-button>
       </div>
@@ -102,9 +197,31 @@
 </template>
 
 <script>
+import { ValidationProvider, extend, configure } from "vee-validate";
+import * as rules from "vee-validate/dist/rules";
+import { messages } from "vee-validate/dist/locale/pt_BR.json";
+import { VMoney } from "v-money";
+
+configure({
+  classes: {
+    valid: "is-valid",
+    invalid: "is-invalid",
+  },
+});
+
+Object.keys(rules).forEach((rule) => {
+  extend(rule, {
+    ...rules[rule], // copies rule configuration
+    message: messages[rule], // assign message
+  });
+});
+
 import { apiBuscaCep } from "@/services.js";
 
 export default {
+  components: {
+    ValidationProvider,
+  },
   data() {
     return {
       isLoading: false,
@@ -117,7 +234,16 @@ export default {
       logradouro: null,
       bairro: null,
       cidade: null,
-
+      estado: null,
+      rendaMensal: null,
+      money: {
+        decimal: ",",
+        thousands: ".",
+        prefix: "",
+        suffix: "",
+        precision: 2,
+        masked: false /* doesn't work with directive */,
+      },
       pets: [
         { value: "cao", text: "Cão" },
         { value: "gato", text: "Gato" },
@@ -129,50 +255,68 @@ export default {
         { value: "LB", text: "Labrador" },
         { value: "PL", text: "Peludo" },
       ],
-      ufs: [
-        { value: "GO", text: "GO" },
-        { value: "SP", text: "SP" },
-        { value: "MG", text: "MG" },
-        { value: "PR", text: "PR" },
-      ],
       show: true,
       selected: null,
       status: null,
       file1: null,
     };
   },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
+  },
+  directives: {
+    money: VMoney,
+  },
   methods: {
-    buscaCep() {
-      if (this.cep.length === 8) {
-        this.isLoading = true;
-        apiBuscaCep.get(this.cep).then((r) => {
-          const dados = r.data;
+    buscaCep(cepFormatado) {
+      this.isLoading = true;
+      apiBuscaCep.get(cepFormatado).then((r) => {
+        const dados = r.data;
 
-          if (dados.erro === true) {
-            this.logradouro = null;
-            this.bairro = null;
-            this.cidade = null;
-            this.estado = null;
+        if (dados.erro === true) {
+          this.logradouro = null;
+          this.bairro = null;
+          this.cidade = null;
+          this.estado = null;
 
-            this.$swal.fire({
-              icon: "error",
-              title: "Oops...",
-              text: "Cep não encontrado, verifique o cep digitado e tente novamente.",
-            });
-            this.isLoading = false;
-          } else {
-            this.logradouro = dados.logradouro;
-            this.bairro = dados.bairro;
-            this.cidade = dados.localidade;
-            this.estado = dados.uf;
-            this.isLoading = false;
-          }
-        });
+          this.$swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Cep não encontrado, verifique o cep digitado e tente novamente.",
+          });
+          this.isLoading = false;
+        } else {
+          this.logradouro = dados.logradouro;
+          this.bairro = dados.bairro;
+          this.cidade = dados.localidade;
+          this.estado = dados.uf;
+          this.isLoading = false;
+        }
+      });
+    },
+  },
+  watch: {
+    cep() {
+      let cepFormatado = this.cep.replace("-", "");
+      if (cepFormatado.length === 8) {
+        this.buscaCep(cepFormatado);
       }
+    },
+    rendaMensal() {
+      let rendaMensal = this.rendaMensal.split(",");
+      this.rendaMensal = rendaMensal[0].replace(".", "");
     },
   },
 };
 </script>
 
 <style>
+.mensagem-error {
+  color: red;
+}
 </style>
